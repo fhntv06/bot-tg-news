a072ee97b7cde24e55c78565415d9436e555fb36
kartemake <a.kuskov@make.st>
2024-05-14 12:53:21 +0700

    wip

--- functions/find_success_work_proxy.py
+++ functions/find_success_work_proxy.py
@@ -5,43 +5,44 @@ import datetime
 
 from variables.common import path_to_proxy_list, path_to_success_work_proxy, url_check_ip
 
+
 def find_success_work_proxy():
-  with open(path_to_proxy_list, 'r', encoding='utf-8') as f:
-    
-    data = json.load(f)
+    with open(path_to_proxy_list, 'r', encoding='utf-8') as f:
 
-    f = open(path_to_success_work_proxy, 'w')
+        data = json.load(f)
 
-    f.write('<============= Start =================>\n')
-    f.write(f" {datetime.datetime.now()}")
-    f.write("\n\n")
+        f = open(path_to_success_work_proxy, 'w')
 
-    for item in data:
+        f.write('<============= Start =================>\n')
+        f.write(f" {datetime.datetime.now()}")
+        f.write("\n\n")
 
-      proxies = {
-        'http': f"{item['protocols'][0]}://{item['ip']}:{item['port']}",
-        'https': f"{item['protocols'][0]}://{item['ip']}:{item['port']}",
-      }
+        for item in data:
 
-      try:
-        response = requests.get(url=url_check_ip, proxies=proxies)
+            proxies = {
+                'http': f"{item['protocols'][0]}://{item['ip']}:{item['port']}",
+                'https': f"{item['protocols'][0]}://{item['ip']}:{item['port']}",
+            }
 
-        if response.status_code == 200:
-          print(f"Success to connect to {proxies['http']}. In process ... .")
+            try:
+                response = requests.get(url=url_check_ip, proxies=proxies)
 
-          soup = BeautifulSoup(response.text, 'lxml')
-          ip = soup.find('div', id='d_clip_button').find('span').text.strip()
+                if response.status_code == 200:
+                    print(f"Success to connect to {proxies['http']}. In process ... .")
 
-          f.write(f" IP: {item['ip']}\n")
-          f.write(f" Protocol: {item['protocols'][0]}\n")
-          f.write(f" Port: {item['port']}\n")
-          f.write(f" IP on site: {ip}\n")
+                    soup = BeautifulSoup(response.text, 'lxml')
+                    ip = soup.find('div', id='d_clip_button').find('span').text.strip()
 
-      except requests.exceptions.ConnectionError:
-        print(f"Failed to connect to {proxies['http']}. Skipping.")
-        continue
+                    f.write(f" IP: {item['ip']}\n")
+                    f.write(f" Protocol: {item['protocols'][0]}\n")
+                    f.write(f" Port: {item['port']}\n")
+                    f.write(f" IP on site: {ip}\n")
 
-      f.write("\n\n")
-      f.write(f" {datetime.datetime.now()}\n")
-      f.write('<============= End =================>\n\n')
-      f.close()
+            except requests.exceptions.ConnectionError:
+                print(f"Failed to connect to {proxies['http']}. Skipping.")
+                continue
+
+            f.write("\n\n")
+            f.write(f" {datetime.datetime.now()}\n")
+            f.write('<============= End =================>\n\n')
+            f.close()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from functions.find_success_work_proxy import find_success_work_proxy
 
+
 def main():
-  find_success_work_proxy()
-  
+    find_success_work_proxy()
+
+
 if __name__ == '__main__':
-  main()
+    main()
