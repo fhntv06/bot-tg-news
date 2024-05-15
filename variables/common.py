23c10443555a974049413a5d9a5db6f4ae1d0059
kartemake <a.kuskov@make.st>
2024-05-15 09:25:27 +0700

    change paths for variables common

--- variables/common.py
+++ variables/common.py
@@ -1,5 +1,6 @@
 import sys
 
-path_to_proxy_list = f'{sys.path[0]}/files/proxy_list.json'
-path_to_success_work_proxy = f'{sys.path[0]}/files/success_work_proxy.txt'
+path_to_config = f'{sys.path[1]}/files/config.ini'
+path_to_proxy_list = f'{sys.path[1]}/files/proxy_list.json'
+path_to_success_work_proxy = f'{sys.path[1]}/files/success_work_proxy.txt'
 url_check_ip = 'https://2ip.ru'
