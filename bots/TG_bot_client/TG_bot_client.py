b81320164c20f01bea37230f490053ebc03388a7
kartemake <a.kuskov@make.st>
2024-05-15 09:26:44 +0700

    added __name__ in TG_bot_client.py

--- bots/TG_bot_client/TG_bot_client.py
+++ bots/TG_bot_client/TG_bot_client.py
@@ -2,13 +2,18 @@ from functions.connect_telegram_client import client
 from functions.channel_participants_search import get_all_messages
 from functions.get_participants_request import get_all_participants
 
+client.start()
+
 
 async def main():
-	url = input("Введите ссылку на канал или чат: ")
-	channel = await client.get_entity(url)
-	await get_all_participants(channel)
-	await get_all_messages(channel)
+    url = input("Введите ссылку на канал или чат: ")
+    channel = await client.get_entity(url)
+    await get_all_participants(channel)
+    await get_all_messages(channel)
 
+
+if __name__ == '__main__':
+    main()
 
 with client:
-	client.loop.run_until_complete(main())
+    client.loop.run_until_complete(main())
